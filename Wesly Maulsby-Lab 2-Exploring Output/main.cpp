b8d8cfcd72db9e1e482b32756ef359c172ca1f89
WeslyM003 <maulsbywes@gmail.com>
2022-08-31 13:20:36 -0500

    Add project files.

---
+++ Wesly Maulsby-Lab 2-Exploring Output/main.cpp
@@ -0,0 +1,58 @@
+/***************************
+Wesly Maulsby
+C++ Fall 2022
+Lab 2
+Exploring Output
+Get familiar with output to command prompt
+*****************************/
+
+//Pre-Proccessor Directives (Calls input output stream and string libraries)
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+//Part one of Lab 2: schedule
+	//Declares "m" as Monday
+	const std::string m = "Monday  10:00 AM  C++";
+	//Declares "t" as Tuesday
+	const std::string t = "Tuesday  7:50 AM Python 10:00 AM C++ ";
+	//Declares "w" as Wednasday
+	const std::string w = "Wednasday  10:00 AM  C++";
+	//Declares "th" as Thursday
+	const std::string th = "Thursday  7:50 AM Python 10:00 AM C++";
+	//Declares "f" as Friday
+	const std::string f = "Friday  10:00 AM  C++";
+	
+	//Calls day variables
+	//Monday
+	std::cout << m;
+	//Tuesday
+	std::cout << std::endl << t;
+	//Wednasday
+	std::cout << std::endl << w;
+	//Thursday
+	std::cout << std::endl << th;
+	//Friday
+	std::cout << std::endl << f;
+
+//Part 2 of Lab 2: Checker Board
+
+	//Declares "one" as the first row of checkers
+	const std::string one = "     *           *           *           * ";
+
+	//Declares "two" as the second row of checkers
+	const std::string two = "*          *           *           *       ";
+
+	//Calls both lines four times times
+	std::cout << std::endl << one;
+	std::cout << std::endl << two;
+	std::cout << std::endl << one;
+	std::cout << std::endl << two;
+	std::cout << std::endl << one;
+	std::cout << std::endl << two;
+	std::cout << std::endl << one;
+	std::cout << std::endl << two;
+	return(0);
+}
